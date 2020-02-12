424dc2c4cb303f54c15c55053f3f1a48ce023e09
Raymond Wong <RaymondLWong@users.noreply.github.com>
2020-02-12 16:17:56 +0000

    use names as well as starters to distinguish generations

--- Generations.py
+++ Generations.py
@@ -1,15 +1,8 @@
-import requests
 import pokebase as pb
 from random import randrange
 from typing import Mapping, List
 
 from Card import Pokemon, create_pokemon
-
-
-def get_available_generations() -> int:
-    # warning, there's an API limit of 100 / minute, so avoid using this function too often
-    results = requests.get('https://pokeapi.co/api/v2/generation/').text
-    return int(results.count)
 
 
 def get_static_generation_counts() -> Mapping[int, int]:
@@ -43,49 +36,40 @@ def get_random_pokemon(gen: int) -> Pokemon:
     return pokemon_card
 
 
-def get_types(pokemon_types: List[dict]) -> List[str]:
-    avail_types = []
-    for pokemon_type in pokemon_types:
-        avail_types.append(pokemon_type.type.name)
-    return avail_types
-
-
-def colour_pokemon_name_by_its_types(name: str, types: List[str]) -> str:
-    # TODO: use 'colorama library to colour the name
-    return name
-
-
-def get_starter_pokemon(gen: int) -> List[dict]:
-    starters = []
-    print(f'Fetching Gen {gen} starters from PokeAPI', end='')
-    for i in range(1, 3*3, 3):
-        pokemon = pb.pokemon(get_poke_id(gen, i))
-        print('.', end='')
-        starters.append(pokemon)
-    print()
-
-    return starters
-
-
-def get_starter_pokemon_names(gen: int) -> List[str]:
-    starter_pokemon = []
-    for starter in get_starter_pokemon(gen):
-        pokemon_types = get_types(starter.types)
-        pokemon_name: str = colour_pokemon_name_by_its_types(starter.name, pokemon_types)
-        starter_pokemon.append(pokemon_name.capitalize())
-    return starter_pokemon
-
-
-def get_static_starter_pokemon_names() -> dict:
+def get_static_generations_and_starters() -> dict:
     # types are always (grass, fire, water)
     starters = {
-        1: ['Bulbasaur', 'Charmander', 'Squirtle'],
-        2: ['Chikorita', 'Cyndaquil', 'Totodile'],
-        3: ['Bulbasaur', 'Charmander', 'Squirtle'],  # same as Gen 1
-        4: ['Chikorita', 'Cyndaquil', 'Totodile'],  # same as Gen 2
-        5: ['Snivy', 'Tepig', 'Oshawott'],
-        6: ['Chespin', 'Fennekin', 'Froakie'],
-        7: ['Rowlet', 'Litten', 'Popplio'],
-        8: ['Grookey', 'Scorbunny', 'Sobble']
+        1: {
+            'names': ['Red', 'Green', 'Blue'],
+            'starters': ['Bulbasaur', 'Charmander', 'Squirtle']
+        },
+        2: {
+            'names': ['Gold', 'Silver', 'Crystal'],
+            'starters': ['Chikorita', 'Cyndaquil', 'Totodile']
+        },
+        3: {
+            'names': ['Ruby', 'Sapphire', 'Emerald'],
+            'starters': ['Treecko', 'Torchic', 'Mudkip']
+        },
+        4: {
+            'names': ['Diamond', 'Pearl', 'Platinum'],
+            'starters': ['Turtwig', 'Chimchar', 'Piplup']
+        },
+        5: {
+            'names': ['Black', 'White'],
+            'starters': ['Snivy', 'Tepig', 'Oshawott']
+        },
+        6: {
+            'names': ['X', 'Y'],
+            'starters': ['Chespin', 'Fennekin', 'Froakie']
+        },
+        7: {
+            'names': ['Sun', 'Moon'],
+            'starters': ['Rowlet', 'Litten', 'Popplio']
+        },
+        8: {
+            'names': ['Sword', 'Shield'],
+            'starters': ['Grookey', 'Scorbunny', 'Sobble']
+        }
     }
     return starters
